cd862e386924344ca66953dbc2d882fb7a053214
Jessica Mulein <jessica@digitaldefiance.org>
2023-12-04 05:50:01 +0000

    try to get things building on a modern system

--- utility/install-oatpp-modules.sh
+++ utility/install-oatpp-modules.sh
@@ -8,11 +8,13 @@ cd tmp
 ##########################################################
 ## install oatpp
 
-MODULE_NAME="oatpp"
+OATPP_MODULE_NAME="oatpp"
+OATPP_MODULE_VERSION="1.3.0" # specify the version for oatpp
 
-git clone --depth=1 https://github.com/oatpp/$MODULE_NAME
+git clone https://github.com/oatpp/$OATPP_MODULE_NAME
+cd $OATPP_MODULE_NAME
+git checkout tags/$OATPP_MODULE_VERSION -b $OATPP_MODULE_VERSION
 
-cd $MODULE_NAME
 mkdir build
 cd build
 
@@ -24,11 +26,13 @@ cd ../../
 ##########################################################
 ## install oatpp-swagger
 
-MODULE_NAME="oatpp-swagger"
+OATPP_SWAGGER_MODULE_NAME="oatpp-swagger"
+OATPP_SWAGGER_MODULE_VERSION="1.3.0" # specify the version for oatpp-swagger
 
-git clone --depth=1 https://github.com/oatpp/$MODULE_NAME
+git clone https://github.com/oatpp/$OATPP_SWAGGER_MODULE_NAME
+cd $OATPP_SWAGGER_MODULE_NAME
+git checkout tags/$OATPP_SWAGGER_MODULE_VERSION -b $OATPP_SWAGGER_MODULE_VERSION
 
-cd $MODULE_NAME
 mkdir build
 cd build
 
